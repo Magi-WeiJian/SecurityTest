bb601249e06c75e0ce7ed859d5139c8c81a9085f
wesgin <wesgin@163.com>
2016-02-13 22:12:30 +0800

    修改了Setup1Activity

--- Security/src/main/java/com/magi/mobilesecurity/activity/Setup1Activity.java
+++ Security/src/main/java/com/magi/mobilesecurity/activity/Setup1Activity.java
@@ -3,6 +3,7 @@ package com.magi.mobilesecurity.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.magi.mobilesecurity.R;
@@ -17,5 +18,10 @@ public class Setup1Activity extends AppCompatActivity {
 
     public void next(View view){
         startActivity(new Intent(this, Setup2Activity.class));
+    }
+
+
+    public void main(){
+        Log.i("TAG", "main: ");
     }
 }
